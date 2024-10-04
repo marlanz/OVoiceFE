4a0d60df17c09a5b3257bc651b6c87be4ce23417
phanh <anhddpse172448@fpt.edu.vn>
2024-10-04 18:05:31 +0700

    add Login page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./root.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
+import Login from "./pages/Login.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
